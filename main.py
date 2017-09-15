4082f1a5cce31914f199d402d0e542faaa6d1d65
Krzysztof Osuch <krzysztof.osuch@eengine.pl>
2017-09-15 19:54:42 +0200

    moving mosquito

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ def initApp():
     size = (W_WIDTH, W_HEIGHT)
     screen = pygame.display.set_mode(size)
     bgImage = pygame.image.load("resources/gfx/background.png").convert()
+    mosquito.image = pygame.image.load("resources/gfx/mosquito.png").convert()
     screen.fill(BLACK)
     screen.blit(bgImage, (0,0))
     pygame.display.flip()
@@ -47,65 +48,49 @@ def initApp():
 colors = [BLACK, WHITE, RED, GREEN, BLUE]
 colorLength = len(colors)
 colorIndex = 0
+keys_pressed = {
+    pygame.K_RIGHT: False, 
+    pygame.K_LEFT: False, 
+    pygame.K_UP: False, 
+    pygame.K_DOWN: False, 
+}
+TIME_MODIFIER = 0.2
 
 initApp()
-
 while appAlive:
+    screen.fill(BLACK)
+    screen.blit(bgImage, (0,0))
+    time = clock.get_time()*TIME_MODIFIER
+#    print("Frame time: %s"%time)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             appAlive = False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                mosquito.acc_x = 10
-            if event.key == pygame.K_LEFT:
-                mosquito.acc_x = -10
-            if event.key == pygame.K_UP:
-                mosquito.acc_y = 10
-            if event.key == pygame.K_DOWN:
-                mosquito.acc_y = -10
-        mosquito.updateForTime()
+            if event.key in keys_pressed:
+                keys_pressed[event.key] = True
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                colorIndex += 1
-                if colorIndex > colorLength - 1:
-                    colorIndex = 0
-                xOffset = 10
-                yOffset = 10
-                screen.fill(colors[colorIndex])
+            if event.key in keys_pressed:
+                keys_pressed[event.key] = False
+        if keys_pressed[pygame.K_RIGHT]:
+            mosquito.acc_x = 1
+        elif keys_pressed[pygame.K_LEFT]:
+            mosquito.acc_x = -1
+        else:
+            mosquito.acc_x = 0
 
-            if event.key == pygame.K_LEFT:
-                colorIndex -= 1
-                if colorIndex < 0:
-                    colorIndex = colorLength - 1
-                xOffset = 10
-                yOffset = 10
-                screen.fill(colors[colorIndex])
-
-            if event.key == pygame.K_DOWN:
-                pygame.draw.line(screen, colors[colorIndex + 1], [0, yOffset], [W_WIDTH, yOffset], 2)
-                yOffset += 10
-                if yOffset >= W_HEIGHT:
-                    yOffset = 10
-
-            if event.key == pygame.K_UP:
-                pygame.draw.line(screen, colors[colorIndex + 1], [xOffset, 0], [xOffset, W_HEIGHT], 2)
-                xOffset += 10
-                if xOffset >= W_WIDTH:
-                    xOffset = 10
-
-            if event.key == pygame.K_d:
-                while xOffset < W_WIDTH and yOffset < W_HEIGHT:
-                    pygame.draw.line(screen, colors[colorIndex + 1], [0, yOffset], [W_WIDTH, yOffset], 2)
-                    pygame.display.flip()
-                    pygame.draw.line(screen, colors[colorIndex + 1], [xOffset, 0], [xOffset, W_HEIGHT], 2)
-                    pygame.display.flip()
-                    yOffset += 10
-                    xOffset += 10
-
-        #InfoText = fontTahoma.render("DBG: Y: " + str(yOffset) + " X: " + str(xOffset), True, BLACK)
-        # screen.blit(InfoText, [W_WIDTH - 132, W_HEIGHT - 30])
+        if keys_pressed[pygame.K_UP]:
+            mosquito.acc_y = -1
+        elif keys_pressed[pygame.K_DOWN]:
+            mosquito.acc_y = 1
+        else:
+            mosquito.acc_y = 0
         
-        pygame.display.flip()
-        clock.tick(60)
+    mosquito.updateForTime(time)
+    screen.blit(mosquito.image, (mosquito.x,mosquito.y))
+    #InfoText = fontTahoma.render("DBG: Y: " + str(yOffset) + " X: " + str(xOffset), True, BLACK)
+    # screen.blit(InfoText, [W_WIDTH - 132, W_HEIGHT - 30])
+    
+    pygame.display.flip()
+    clock.tick()
 
 pygame.quit()
