7176ce271f87d38f6ea41e718bdba787d55a096d
Jakub Czerwiński <jakub.czerwinski@eengine.pl>
2017-09-15 17:29:38 +0200

    added main.py

---
+++ main.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import pygame, time
+
+# GLOBALS
+W_WIDTH = 640
+W_HEIGHT = 480
+
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+screen = 0
+clock = 0
+mainFont = 0
+xOffset = 0
+yOffset = 0
+appAlive = False
+
+bgImage = 0
+
+
+def initApp():
+    """Initialize app"""
+    global screen, appAlive, clock, mainFont, bgImage
+
+    pygame.init()
+    pygame.display.set_caption("Graphic enabled!")
+    clock = pygame.time.Clock()
+    mainFont = pygame.font.SysFont('Tahoma', 16, False, False)
+    size = (W_WIDTH, W_HEIGHT)
+    screen = pygame.display.set_mode(size)
+    bgImage = pygame.image.load("resources/background.jpg").convert()
+    screen.fill(BLACK)
+    screen.blit(bgImage)
+    pygame.display.flip()
+    appAlive = True
+
+
+colors = [BLACK, WHITE, RED, GREEN, BLUE]
+colorLength = len(colors)
+colorIndex = 0
+
+initApp()
+
+while appAlive:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            appAlive = False
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                colorIndex += 1
+                if colorIndex > colorLength - 1:
+                    colorIndex = 0
+                xOffset = 10
+                yOffset = 10
+                screen.fill(colors[colorIndex])
+
+            if event.key == pygame.K_LEFT:
+                colorIndex -= 1
+                if colorIndex < 0:
+                    colorIndex = colorLength - 1
+                xOffset = 10
+                yOffset = 10
+                screen.fill(colors[colorIndex])
+
+            if event.key == pygame.K_DOWN:
+                pygame.draw.line(screen, colors[colorIndex + 1], [0, yOffset], [W_WIDTH, yOffset], 2)
+                yOffset += 10
+                if yOffset >= W_HEIGHT:
+                    yOffset = 10
+
+            if event.key == pygame.K_UP:
+                pygame.draw.line(screen, colors[colorIndex + 1], [xOffset, 0], [xOffset, W_HEIGHT], 2)
+                xOffset += 10
+                if xOffset >= W_WIDTH:
+                    xOffset = 10
+
+            if event.key == pygame.K_d:
+                while xOffset < W_WIDTH and yOffset < W_HEIGHT:
+                    pygame.draw.line(screen, colors[colorIndex + 1], [0, yOffset], [W_WIDTH, yOffset], 2)
+                    pygame.display.flip()
+                    time.sleep(0.03)
+                    pygame.draw.line(screen, colors[colorIndex + 1], [xOffset, 0], [xOffset, W_HEIGHT], 2)
+                    pygame.display.flip()
+                    time.sleep(0.03)
+                    yOffset += 10
+                    xOffset += 10
+
+        InfoText = fontTahoma.render("DBG: Y: " + str(yOffset) + " X: " + str(xOffset), True, BLACK)
+        screen.blit(InfoText, [W_WIDTH - 130, W_HEIGHT - 30])
+
+        pygame.display.flip()
+        clock.tick(60)
+
+pygame.quit()
