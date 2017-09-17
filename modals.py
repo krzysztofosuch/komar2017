1094cd4645603bb2de7b63dfcd57e10b4e6be1fe
Jakub Czerwiński <jakub.czerwinski@eengine.pl>
2017-09-17 10:34:57 +0200

    borders

--- modals.py
+++ modals.py
@@ -12,10 +12,10 @@ class Modals:
         rect = pygame.draw.rect(self.screen, (0,0,0), self.getRect(), 0)
         print(self.getRect())
         ass = pygame.image.load("resources/gfx/pupa/PUPA_CIEŃ_pupa.png").convert()
-        self.rect.blit(ass, (100, 100))
+        self.screen.blit(ass, (60, 60))
         # run_view = pygame.image.load("resources/gfx/runscreen.png").convert()
 
     def getRect(self):
-        lu = (100, 100)
+        lu = (50, 50)
 
-        return (lu, (self.screen_size[0] - 200, self.screen_size[1] - 200))
+        return (lu, (self.screen_size[0] - 100, self.screen_size[1] - 100))
